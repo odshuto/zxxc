ecbde953a5244d74d67d161f9f1802616f397d85
Odshuto <odshuto@icloud.com>
2023-01-05 20:58:08 +0500

    home_work

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+//6 -> да
+
+// Дз №10
+int number_3 = 0; 
+Console.WriteLine("№10");
+Console.Write("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+number_3 = number % 100 / 10;
+Console.WriteLine($"2-e число:{number_3}");
+
+// №13
+int number_1 = 0; 
+Console.WriteLine("№10");
+Console.Write("Введите трехзначное число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 < 100)
+    Console.WriteLine("Это число не трехзначное)");
+else
+    number_1 = number2 % 100 % 10;
+    Console.WriteLine($"3-e число:{number_1}");
+
+//№15
+Console.WriteLine("№15");
+Console.Write("Введите день недели по счету : ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number3 > 5)
+    Console.WriteLine("Yes:)");
+else
+    Console.WriteLine("No:(");
